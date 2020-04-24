e3228b6a8a22c4da432c889c4b49b87923245d4e
Ta180m <ta180m@gmail.com>
2020-04-24 17:15:03 -0500

    build

---
+++ arm9/build/inihandler.d
@@ -0,0 +1,5 @@
+inihandler.o: \
+ C:/Users/shengyue/Documents/GitHub/Make-Forwarder-Dsi/arm9/source/inihandler.cpp \
+ C:/Users/shengyue/Documents/GitHub/Make-Forwarder-Dsi/arm9/source/inihandler.h
+
+C:/Users/shengyue/Documents/GitHub/Make-Forwarder-Dsi/arm9/source/inihandler.h:
